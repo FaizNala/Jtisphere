8f46f3dfc8621df9c27b009a0bbce97adb5be443
FaizNala <125951962+FaizNala@users.noreply.github.com>
2024-11-03 14:10:37 +0700

    Kategori Menu

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\kategoriController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
@@ -33,7 +34,7 @@ Route::post('register', [AuthController::class, 'postRegister']);
 Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
 
 // Routes that require authentication
-Route::middleware(['auth'])->group(function() {
+Route::middleware(['auth'])->group(function () {
 
     // Welcome route
     Route::get('/', [WelcomeController::class, 'index']);
@@ -45,7 +46,7 @@ Route::middleware(['auth'])->group(function() {
             Route::post('/list', [LevelController::class, 'list']);
             Route::get('/create_ajax', [LevelController::class, 'create_ajax']);
             Route::post('/ajax', [LevelController::class, 'store_ajax']);
-            Route::get('/{id}/show_ajax', [LevelController::class,'show_ajax']);
+            Route::get('/{id}/show_ajax', [LevelController::class, 'show_ajax']);
             Route::get('/{id}/edit_ajax', [LevelController::class, 'edit_ajax']);
             Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']);
             Route::get('/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']);
@@ -59,12 +60,12 @@ Route::middleware(['auth'])->group(function() {
 
     // User routes
     Route::middleware(['authorize:ADM'])->group(function () {
-        Route::group(['prefix' => 'user'], function() {
+        Route::group(['prefix' => 'user'], function () {
             Route::get('/', [UserController::class, 'index']);
             Route::post('/list', [UserController::class, 'list']);
             Route::get('/create_ajax', [UserController::class, 'create_ajax']);
             Route::post('/ajax', [UserController::class, 'store_ajax']);
-            Route::get('/{id}/show_ajax', [UserController::class,'show_ajax']);
+            Route::get('/{id}/show_ajax', [UserController::class, 'show_ajax']);
             Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
             Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);
             Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);
@@ -76,8 +77,27 @@ Route::middleware(['auth'])->group(function() {
         });
     });
 
+    // Kategori routes
+    Route::middleware(['authorize:ADM,MNG,STF'])->group(function () {
+        Route::group(['prefix' => 'kategori'], function () {
+            Route::get('/', [KategoriController::class, 'index']);
+            Route::post('/list', [KategoriController::class, 'list']);
+            Route::get('/create_ajax', [KategoriController::class, 'create_ajax']);
+            Route::post('/ajax', [KategoriController::class, 'store_ajax']);
+            Route::get('/{id}/show_ajax', [KategoriController::class, 'show_ajax']);
+            Route::get('/{id}/edit_ajax', [KategoriController::class, 'edit_ajax']);
+            Route::put('/{id}/update_ajax', [KategoriController::class, 'update_ajax']);
+            Route::get('/{id}/delete_ajax', [KategoriController::class, 'confirm_ajax']);
+            Route::delete('/{id}/delete_ajax', [KategoriController::class, 'delete_ajax']);
+            Route::get('/import', [KategoriController::class, 'import']);
+            Route::post('/import_ajax', [KategoriController::class, 'import_ajax']);
+            Route::get('/export_excel', [KategoriController::class, 'export_excel']);
+            Route::get('/export_pdf', [KategoriController::class, 'export_pdf']);
+        });
+    });
+
     Route::middleware('authorize:ADM,MNG,STF')->group(function () {
-        Route::group(['prefix' => 'profile'], function() {
+        Route::group(['prefix' => 'profile'], function () {
             Route::get('/', [ProfileController::class, 'index']);
             Route::get('/edit_ajax', [ProfileController::class, 'edit_ajax']);
             Route::post('/update_ajax', [ProfileController::class, 'update_ajax']);
