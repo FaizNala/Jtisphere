19edf53224198c27e105e188702eb45a3790521c
AchmadDanniMPA <gundul321mmm@gmail.com>
2024-12-08 15:24:53 +0700

    Add new Dosen

--- WEB/database/seeders/UserSeeder.php
+++ WEB/database/seeders/UserSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -15,25 +14,18 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            [
-                'user_id' => 1,
-                'username' => 'Administrator',
-                'password' => Hash::make(12345),
-                'created_at' => now()
-            ],
-            [
-                'user_id' => 2,
-                'username' => 'Pimpinan',
-                'password' => Hash::make(12345),
-                'created_at' => now()
-            ],
-            [
-                'user_id' => 3,
-                'username' => 'Dosen',
-                'password' => Hash::make(12345),
-                'created_at' => now()
-            ]
+            ['user_id' => 1, 'username' => 'Administrator', 'password' => Hash::make('12345'), 'created_at' => now()],
+            ['user_id' => 2, 'username' => 'Pimpinan', 'password' => Hash::make('12345'), 'created_at' => now()],
+            ['user_id' => 3, 'username' => 'Dosen', 'password' => Hash::make('12345'), 'created_at' => now()],
+            ['user_id' => 4, 'username' => 'Ahmadi', 'password' => Hash::make('12345'), 'created_at' => now()],
+            ['user_id' => 5, 'username' => 'Ely', 'password' => Hash::make('12345'), 'created_at' => now()],
+            ['user_id' => 6, 'username' => 'Erfan', 'password' => Hash::make('12345'), 'created_at' => now()],
+            ['user_id' => 7, 'username' => 'Faisal', 'password' => Hash::make('12345'), 'created_at' => now()],
+            ['user_id' => 8, 'username' => 'Gunawan', 'password' => Hash::make('12345'), 'created_at' => now()],
+            ['user_id' => 9, 'username' => 'Hendra', 'password' => Hash::make('12345'), 'created_at' => now()],
+            ['user_id' => 10, 'username' => 'Imam', 'password' => Hash::make('12345'), 'created_at' => now()],
         ];
+
         DB::table('m_user')->insert($data);
     }
 }
