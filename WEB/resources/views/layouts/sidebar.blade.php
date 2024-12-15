33f1e11329a8d600dba674efd2f76165116ebc49
FaizNala <125951962+FaizNala@users.noreply.github.com>
2024-12-16 01:18:04 +0700

    sidebar custom

--- WEB/resources/views/layouts/sidebar.blade.php
+++ WEB/resources/views/layouts/sidebar.blade.php
@@ -1,13 +1,13 @@
-<div class="sidebar bg-dark">
+<div class="sidebar bg-dark" style="background-color: #264585 !important;">
     <!-- User Info -->
     @if (Auth::check() && Auth::user()->dosen)
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="user-panel pb-3 mb-3 d-flex">
             <div class="image">
                 <img src="{{ Auth::user()->dosen->avatar ?? asset('default-avatar.png') }}"
                     class="rounded-circle" alt="User Image">
             </div>
             <div class="info">
-                <a href="{{ url('/profile') }}" class="d-block">
+                <a href="{{ url('/profile') }}" class="d-block text-white">
                     {{ Str::words(Auth::user()->dosen->nama, 3, '') }}
                 </a>
             </div>
@@ -15,7 +15,7 @@
     @endif
 
     <!-- SidebarSearch Form -->
-    <div class="form-inline mt-2">
+    {{-- <div class="form-inline mt-2">
         <div class="input-group" data-widget="sidebar-search">
             <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
             <div class="input-group-append">
@@ -24,7 +24,7 @@
                 </button>
             </div>
         </div>
-    </div>
+    </div> --}}
     <!-- Sidebar Menu  -->
     <nav class="mt-2">
         @php
@@ -40,35 +40,35 @@
                 <!-- Dashboard Link -->
                 <li class="nav-item">
                     <a href="{{ url('/home') }}" class="nav-link {{ $activeMenu == 'dashboard' ? 'active' : '' }}">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>Dashboard</p>
+                        <i class="nav-icon fas fa-tachometer-alt text-white"></i>
+                        <p class="text-white">Dashboard</p>
                     </a>
                 </li>
                 <!-- Kalender Link -->
                 <li class="nav-item">
                     <a href="{{ url('/kalender') }}" class="nav-link {{ $activeMenu == 'kalender' ? 'active' : '' }}">
-                        <i class="nav-icon fas fa-calendar-alt"></i>
-                        <p>Kalender</p>
+                        <i class="nav-icon fas fa-calendar-alt text-white"></i>
+                        <p class="text-white">Kalender</p>
                     </a>
                 </li>
                 <!-- Data Pengguna Section -->
-                <li class="nav-header">Data User</li>
+                <li class="nav-header text-white">Data User</li>
                 <li class="nav-item">
                     <a href="{{ url('/user') }}" class="nav-link {{ $activeMenu == 'user' ? 'active' : '' }}">
-                        <i class="nav-icon far fa-user"></i>
-                        <p>Data User</p>
+                        <i class="nav-icon far fa-user text-white"></i>
+                        <p class="text-white">Data User</p>
                     </a>
                 </li>
                 <!-- Data Kegiatan Section -->
-                <li class="nav-header">Data Kegiatan</li>
+                <li class="nav-header text-white">Data Kegiatan</li>
                 <li
                     class="nav-item has-treeview {{ in_array($activeMenu, ['kategori', 'periode', 'peran', 'kegiatan']) ? 'menu-open' : '' }}">
                     <a href="#"
                         class="nav-link {{ in_array($activeMenu, ['kategori', 'periode', 'peran', 'kegiatan']) ? 'active' : '' }}">
-                        <i class="nav-icon fas fa-folder"></i>
-                        <p>
+                        <i class="nav-icon fas fa-folder text-white"></i>
+                        <p class="text-white">
                             Data Kegiatan
-                            <i class="right fas fa-angle-left"></i>
+                            <i class="right fas fa-angle-left text-white"></i>
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
@@ -82,8 +82,8 @@
                         <li class="nav-item">
                             <a href="{{ url('/periode') }}"
                                 class="nav-link {{ $activeMenu == 'periode' ? 'active' : '' }}">
-                                <i class="nav-icon fas fa-calendar-alt"></i>
-                                <p>Periode Kegiatan</p>
+                                <i class="nav-icon fas fa-calendar-alt text-white"></i>
+                                <p class="text-white">Periode Kegiatan</p>
                             </a>
                         </li>
                         <!-- <li class="nav-item">
@@ -96,18 +96,18 @@
                         <li class="nav-item">
                             <a href="{{ url('/kegiatan') }}"
                                 class="nav-link {{ $activeMenu == 'kegiatan' ? 'active' : '' }}">
-                                <i class="nav-icon far fa-list-alt"></i>
-                                <p>Data Kegiatan</p>
+                                <i class="nav-icon far fa-list-alt text-white"></i>
+                                <p class="text-white">Data Kegiatan</p>
                             </a>
                         </li>
                     </ul>
                 </li>
-                <li class="nav-header">Statistik</li>
+                <li class="nav-header text-white">Statistik</li>
                 <li class="nav-item">
                     <a href="{{ url('/statistik') }}"
                         class="nav-link {{ $activeMenu == 'statistik' ? 'active' : '' }}">
-                        <i class="nav-icon fas fa-chart-line"></i>
-                        <p>Statistik Dosen</p>
+                        <i class="nav-icon fas fa-chart-line text-white"></i>
+                        <p class="text-white">Statistik Dosen</p>
                     </a>
                 </li>
             </ul>
@@ -119,39 +119,39 @@
                 <!-- Dashboard Link -->
                 <li class="nav-item">
                     <a href="{{ url('/') }}" class="nav-link {{ $activeMenu == 'dashboard' ? 'active' : '' }}">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>Dashboard</p>
+                        <i class="nav-icon fas fa-tachometer-alt text-white"></i>
+                        <p class="text-white">Dashboard</p>
                     </a>
                 </li>
                 <!-- Kalender Link -->
                 <li class="nav-item">
                     <a href="{{ url('/kalender') }}" class="nav-link {{ $activeMenu == 'kalender' ? 'active' : '' }}">
-                        <i class="nav-icon fas fa-calendar-alt"></i>
-                        <p>Kalender</p>
+                        <i class="nav-icon fas fa-calendar-alt text-white"></i>
+                        <p class="text-white">Kalender</p>
                     </a>
                 </li>
                 <!-- Data Pengguna Section -->
-                <li class="nav-header">Data Pengguna</li>
+                <li class="nav-header text-white">Data Pengguna</li>
                 <li class="nav-item">
                     <a href="{{ url('/user') }}" class="nav-link {{ $activeMenu == 'user' ? 'active' : '' }}">
-                        <i class="nav-icon far fa-user"></i>
-                        <p>Data Pengguna</p>
+                        <i class="nav-icon far fa-user text-white"></i>
+                        <p class="text-white">Data Pengguna</p>
                     </a>
                 </li>
                 <!-- Data Kegiatan Section -->
-                <li class="nav-header">Data Kegiatan</li>
+                <li class="nav-header text-white">Data Kegiatan</li>
                 <li class="nav-item">
                     <a href="{{ url('/kegiatan') }}" class="nav-link {{ $activeMenu == 'kegiatan' ? 'active' : '' }}">
-                        <i class="nav-icon far fa-list-alt"></i>
-                        <p>Data Kegiatan</p>
+                        <i class="nav-icon far fa-list-alt text-white"></i>
+                        <p class="text-white">Data Kegiatan</p>
                     </a>
                 </li>
-                <li class="nav-header">Statistik</li>
+                <li class="nav-header text-white">Statistik</li>
                 <li class="nav-item">
                     <a href="{{ url('/statistik') }}"
                         class="nav-link {{ $activeMenu == 'statistik' ? 'active' : '' }}">
-                        <i class="nav-icon fas fa-chart-line"></i>
-                        <p>Statistik Dosen</p>
+                        <i class="nav-icon fas fa-chart-line text-white"></i>
+                        <p class="text-white">Statistik Dosen</p>
                     </a>
                 </li>
             </ul>
@@ -165,7 +165,7 @@
                 <li class="nav-item">
                     <a href="{{ url('/') }}" class="nav-link {{ $activeMenu == 'dashboard' ? 'active' : '' }}">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>Dashboard</p>
+                        <p class="text-white">Dashboard</p>
                     </a>
                 </li>
 
@@ -174,18 +174,18 @@
                     <a href="{{ url('/kalender') }}"
                         class="nav-link {{ $activeMenu == 'kalender' ? 'active' : '' }}">
                         <i class="nav-icon fas fa-calendar-alt"></i>
-                        <p>Kalender</p>
+                        <p class="text-white">Kalender</p>
                     </a>
                 </li>
 
                 <!-- Kegiatan Link -->
-                <li class="nav-header">Data Kegiatan</li>
+                <li class="nav-header text-white">Data Kegiatan</li>
                 <li
                     class="nav-item has-treeview {{ in_array($activeMenu, ['kegiatan', 'agenda_dosen']) ? 'menu-open' : '' }}">
                     <a href="#"
                         class="nav-link {{ in_array($activeMenu, ['kegiatan', 'agenda_dosen']) ? 'active' : '' }}">
                         <i class="nav-icon fas fa-folder"></i>
-                        <p>
+                        <p class="text-white">
                             Data Kegiatan
                             <i class="right fas fa-angle-left"></i>
                         </p>
@@ -195,14 +195,14 @@
                             <a href="{{ url('/kegiatan_dosen') }}"
                                 class="nav-link {{ $activeMenu == 'kegiatan' ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-list-alt"></i>
-                                <p>Kegiatan</p>
+                                <p class="text-white">Kegiatan</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="{{ url('/agenda_dosen') }}"
                                 class="nav-link {{ $activeMenu == 'agenda_dosen' ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-clipboard-list"></i>
-                                <p>Agenda Dosen</p>
+                                <p class="text-white">Agenda Dosen</p>
                             </a>
                         </li>
                     </ul>
